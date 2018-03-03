d18d1177988f66563616e13c1a1d75c56d3d1915
termslang <m.baynov@gmail.com>
2018-03-03 16:55:48 +0300

    fix

--- trustdao.sol
+++ trustdao.sol
@@ -13,18 +13,23 @@ To untrust a leader:
 
 contract TrustDAO {
     mapping (address => mapping (uint256 => address)) public followers;
+    mapping (address => uint256) public followersBalance;
     mapping (address => address) public trustedLeader;
 
     function trust(address leader, uint256 spot) {
+        if (spot == 0) revert();
         if (trustedLeader[msg.sender] != 0x00 || trustedLeader[msg.sender] == leader) return;
         uint256 i=spot;
         while (followers[leader][spot] > 0x01) ++i;
         followers[leader][spot] = msg.sender;
+        followersBalance[leader] += balanceOf(msg.sender);
         trustedLeader[msg.sender] = leader;
     }
     function untrust(uint256 spot) {
+        if (spot == 0) revert();
         address leader = trustedLeader[msg.sender];
         if (followers[leader][spot] == msg.sender) followers[leader][spot] = 0x01;
+        followersBalance[leader] -= balanceOf(msg.sender);
         trustedLeader[msg.sender] = 0x00;
     }
 
@@ -35,12 +40,7 @@ contract TrustDAO {
         if (trustedLeader[leader] == 0x00) {
           b = balanceOf(leader);
         } else b = 0;
-        uint256 i=1;
-        while (followers[leader][i] != 0x00) {
-            b += balanceOf(followers[leader][i]);
-            ++i;
-        }
-        return b;
+        return b + followersBalance[leader];
     }
 
     // Returns 0 if already trusted, return spot otherwise
@@ -64,10 +64,7 @@ contract TrustDAO {
     }
 
 
-
-// Standard ERC20 below
-
-
+// ERC20
 
 event Transfer(address indexed _from, address indexed _to, uint256 _value);
 event Approval(address indexed _owner, address indexed _spender, uint256 _value);
@@ -89,7 +86,10 @@ event Approval(address indexed _owner, address indexed _spender, uint256 _value)
         decimals = 18;
         symbol = "TN1";
     }
+
+    // Unable to transfer if has trustedLeader
     function transfer(address _to, uint256 _value) public returns (bool success) {
+        if (trustedLeader[msg.sender] != 0x00) return false;
         require(balances[msg.sender] >= _value);
         balances[msg.sender] -= _value;
         balances[_to] += _value;
@@ -97,6 +97,7 @@ event Approval(address indexed _owner, address indexed _spender, uint256 _value)
         return true;
     }
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        if (trustedLeader[msg.sender] != 0x00) return false;
         uint256 allowance = allowed[_from][msg.sender];
         require(balances[_from] >= _value && allowance >= _value);
         balances[_to] += _value;
